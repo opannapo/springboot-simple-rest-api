8a582d4affe3098c90037b0479762da0ccf88d81
Taufan <taufan.alfazri@gmail.com>
2020-09-09 22:27:37 +0700

    Replace default handler for HttpRequestMethodNotSupportedException to be custom json & error handling

--- napofirestore/src/main/java/com/example/napofirestore/api/controller/ExceptionControllerAdvice.java
+++ napofirestore/src/main/java/com/example/napofirestore/api/controller/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package com.example.napofirestore.api.controller;
 import com.example.napofirestore.api.common.exceptions.HeaderRequestException;
 import com.example.napofirestore.api.templates.response.Res;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,14 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class ExceptionControllerAdvice extends BaseRestController {
     @ExceptionHandler(value = {HeaderRequestException.class})
-    protected Res headerRequestException(HttpServletRequest request, RuntimeException ex) {
+    protected ResponseEntity<Res> headerRequestException(HttpServletRequest request, RuntimeException ex) {
         String msg = "Error ye " + ex.getMessage();
-        return outFail(HttpStatus.BAD_REQUEST.value(), msg);
+        Res res = outFail(HttpStatus.BAD_REQUEST.value(), msg);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
-    protected Res httpRequestMethodNotSupportedException(HttpServletRequest request, RuntimeException ex) {
+    protected ResponseEntity<Res> httpRequestMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException ex) {
         String msg = "Error ye " + ex.getMessage();
-        return outFail(HttpStatus.BAD_GATEWAY.value(), msg);
+        Res res = outFail(HttpStatus.BAD_REQUEST.value(), msg);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 }
