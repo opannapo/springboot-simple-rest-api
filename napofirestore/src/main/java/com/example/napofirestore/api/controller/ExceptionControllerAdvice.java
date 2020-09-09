7c4e758494948f5741ee56d1752c11415ce3f611
Taufan <taufan.alfazri@gmail.com>
2020-09-09 22:12:11 +0700

    Update config hibernate error mysqldriver timezone. Add default error handler for Exceptio HttpRequestMethodNotSupportedException & Custom Exception

--- napofirestore/src/main/java/com/example/napofirestore/api/controller/ExceptionControllerAdvice.java
+++ napofirestore/src/main/java/com/example/napofirestore/api/controller/ExceptionControllerAdvice.java
@@ -2,6 +2,8 @@ package com.example.napofirestore.api.controller;
 
 import com.example.napofirestore.api.common.exceptions.HeaderRequestException;
 import com.example.napofirestore.api.templates.response.Res;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,8 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class ExceptionControllerAdvice extends BaseRestController {
     @ExceptionHandler(value = {HeaderRequestException.class})
-    protected Res handleHeaderException(HttpServletRequest request, RuntimeException ex) {
+    protected Res headerRequestException(HttpServletRequest request, RuntimeException ex) {
         String msg = "Error ye " + ex.getMessage();
-        return outFail(200, msg);
+        return outFail(HttpStatus.BAD_REQUEST.value(), msg);
+    }
+
+    @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
+    protected Res httpRequestMethodNotSupportedException(HttpServletRequest request, RuntimeException ex) {
+        String msg = "Error ye " + ex.getMessage();
+        return outFail(HttpStatus.BAD_GATEWAY.value(), msg);
     }
 }
