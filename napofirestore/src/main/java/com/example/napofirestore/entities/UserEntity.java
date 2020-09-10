97f1d20cfa5715d08ab2db0d76c64424595815ad
Taufan <taufan.alfazri@gmail.com>
2020-09-11 01:17:07 +0700

    Implementation LOMBOK on Model UserEntity

--- napofirestore/src/main/java/com/example/napofirestore/entities/UserEntity.java
+++ napofirestore/src/main/java/com/example/napofirestore/entities/UserEntity.java
@@ -1,105 +1,44 @@
 package com.example.napofirestore.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
-@Table(name = "user", schema = "napo-firestore", catalog = "")
+@Table(name = "user", schema = "napo-firestore")
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
 public class UserEntity {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String profilePicture;
-    private Integer birthDate;
-    private String phone;
-    private String email;
-
     @Id
     @Column(name = "id")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    private int id;
 
     @Basic
     @Column(name = "first_name")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    private String firstName;
 
     @Basic
     @Column(name = "last_name")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    private String lastName;
 
     @Basic
     @Column(name = "profile_picture")
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
+    private String profilePicture;
 
     @Basic
     @Column(name = "birth_date")
-    public Integer getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Integer birthDate) {
-        this.birthDate = birthDate;
-    }
+    private Integer birthDate;
 
     @Basic
     @Column(name = "phone")
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    private String phone;
 
     @Basic
     @Column(name = "email")
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserEntity that = (UserEntity) o;
-        return id == that.id &&
-                Objects.equals(firstName, that.firstName) &&
-                Objects.equals(lastName, that.lastName) &&
-                Objects.equals(profilePicture, that.profilePicture) &&
-                Objects.equals(birthDate, that.birthDate) &&
-                Objects.equals(phone, that.phone) &&
-                Objects.equals(email, that.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, profilePicture, birthDate, phone, email);
-    }
+    private String email;
 }
